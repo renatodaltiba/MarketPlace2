123d3d540b76eebd845358ab603df9a698a59d75
Code Cake <49502646+codecakesoft@users.noreply.github.com>
2019-08-17 21:29:09 -0400

    EDITADO

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -7,13 +7,15 @@ class PurchaseController {
     const { ad, content } = req.body
 
     const purchaseAd = await Ad.findById(ad).populate('author')
+
     const user = await User.findById(req.userId)
 
     await Mail.sendMail({
       from: '"Renato Daltiba Schneider" <renatodaltiba@gmail.com.br>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -1,6 +1,21 @@
 const nodemailer = require('nodemailer')
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars')
+const exphbs = require('express-handlebars')
 const mailConfig = require('../../config/mail')
 
 const transport = nodemailer.createTransport(mailConfig)
+const viewPath = path.join(__dirname, '..', 'views', 'emails')
+
+transport.use(
+  'compile',
+  hbs({
+    viewEngine: exphbs.create({
+      partialsDir: path.join(viewPath, 'partials')
+    }),
+    viewPath,
+    extName: '.hbs'
+  })
+)
 
 module.exports = transport
